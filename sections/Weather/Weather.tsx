ce6e0f0c066a04321f9422119133f0e6d4cd8b1d
italofg <italo@agenciafg.com.br>
2024-03-26 21:25:10 -0300

    commit second

--- sections/Weather/Weather.tsx
+++ sections/Weather/Weather.tsx
@@ -1,11 +1,15 @@
-import {Temperature} from "apps/weather/loaders/temperature.ts";
+import { Temperature } from "apps/weather/loaders/temperature.ts";
 
-export interface Props{
-    temperature?:  Temperature | null;
+export interface Props {
+  temperature?: Temperature | null;
 }
 
-function Weather({temperature}:  Props) {
-    return <h1 class="flex justify-center">No brasil a temperatura está em {temperature?.celsius} °C</h1>;
+function Weather({ temperature }: Props) {
+  return (
+    <h1 class="flex justify-center">
+      No brasil a temperatura está em {temperature?.celsius} °C
+    </h1>
+  );
 }
 
 export default Weather;
